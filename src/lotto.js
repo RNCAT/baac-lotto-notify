83260eb029c3d2f40e905f86bda05cebfc9113c1
RNCAT <jaruwat.works@gmail.com>
2021-11-17 17:11:18 +0700

    :poop: try something

--- src/lotto.js
+++ src/lotto.js
@@ -3,19 +3,21 @@ const cheerio = require('cheerio')
 const https = require('https')
 
 const getLotto = async (lottoList) => {
-  const agent = https.Agent({
-    rejectUnauthorized: false,
-  })
+  const config = {
+    httpsAgent: new https.Agent({ keepAlive: true, rejectUnauthorized: false }),
+  }
 
   let ResultList = []
 
   for (let i = 0; i < lottoList.length; i++) {
     const URL = `http://www.baac.or.th/salak/content-lotto.php?lotto_group=${lottoList[i].lottoGroup}&start_no=${lottoList[i].startNo}&stop_no=${lottoList[i].stopNo}&inside=7`
-    const { data } = await axios.get(URL, { httpsAgent: agent })
+    const { data } = await axios.get(URL, config)
+
+    const results = await scrape(data)
 
     const result = {
       lottoNo: `${lottoList[i].startNo}-${lottoList[i].stopNo}`,
-      result: await scrape(data),
+      result: results,
     }
 
     console.log(result)
